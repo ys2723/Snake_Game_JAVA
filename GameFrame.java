40e18ed5d9779a7a10697c4dc4bfc915055dfabd
Yash Tripathi <yashtripathi2205@gmail.com>
2023-09-27 09:54:38 +0530

    10

--- GameFrame.java
+++ GameFrame.java
@@ -13,3 +13,10 @@ public class GameFrame extends JFrame{
     */
 
     this.add(new GamePanel());
+
+    this.setTitle("Snake");
+    this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    this.setResizable(false);
+    this.pack();
+    this.setVisible(true);
+    this.setLocationRelativeTo(null);
