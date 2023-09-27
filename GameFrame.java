bbb3ed1826b855145a6e0a6e2ada46c486fdbe7a
Yash Tripathi <yashtripathi2205@gmail.com>
2023-09-27 09:53:51 +0530

    8

--- GameFrame.java
+++ GameFrame.java
@@ -3,4 +3,11 @@ import javax.swing.JFrame;
 public class GameFrame extends JFrame{
 
   GameFrame(){
-    
+    /*
+        GamePanel Panel = new GamePanel();
+        this.addPanel;                     (23 September 2023, 2000 hrs right now hehe).
+
+        This work above can be done by just one line,
+        and that line is:
+        this.add(new GamePanel());
+    */
